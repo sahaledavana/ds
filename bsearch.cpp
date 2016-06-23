81b21997fcb06e82decaade072946a03f486c8d3
Sahal <sahal@sahal-300V3A-300V4A-300V5A-200A4B-200A5Bsahal>
2016-06-23 15:42:01 +0530

    added

--- bsearch.cpp
+++ bsearch.cpp
@@ -1,9 +1,39 @@
+#include<iostream>
+using namespace std;
 int main()
 {
 
 
-	int i;
-        int p;
-	return 0;
+	int n,first,last,mid,s;
+     cout<<"enter the size\n";
+     cin>>n;
+     int a[n];
+     cout<<"\nenter the array elements\n"; 
+     for(int i=0;i<n;i++)
+      cin>>a[i];
+     first=0;
+     last=n-1;
+      
+     cout<<"etne rthe search element\n";
+    cin>>s;
+      for(int i=0;i<n;i++)
+{
+    mid=(first+last)/2;
+       if(a[mid]==s)
+     {
+       cout<<"found @"<<mid<<"th postion\n";
+       return 0;
+     }
+    else if(a[mid]>s)
+    last=mid-1;
+    else 
+    first=mid+1;
+}
+cout<<"not found";
+    
+      
+       
+      
+//	return 0;
 }
 
